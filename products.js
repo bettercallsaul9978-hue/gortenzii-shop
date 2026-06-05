600614a0593f8fe02595ae96246aa81cd09b0639
bettercallsaul9978-hue <bettercallsaul9978@gmail.com>
2026-06-06 00:57:48 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,443 @@
+const products = [
+
+/* ГОРТЕНЗИИ МЕТЕЛЬЧАТЫЕ */
+
+{
+    name: "Гортензия метельчатая Ванила Фрайс",
+    price: "7 500 ₽",
+    category: "Гортензии",
+    image: "images/vanila-frais.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Металлика",
+    price: "1 500 ₽",
+    category: "Гортензии",
+    image: "images/metallika.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Самарская Лидия",
+    price: "2 500 ₽",
+    category: "Гортензии",
+    image: "images/samarskaya-lidiya.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Сандай Фрайс",
+    price: "1 800 ₽",
+    category: "Гортензии",
+    image: "images/sunday-frais.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Лаймлайт",
+    price: "1 800 ₽",
+    category: "Гортензии",
+    image: "images/limelight.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Фрайс Мельба",
+    price: "1 500 ₽",
+    category: "Гортензии",
+    image: "images/fraise-melba.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Самарская Лидия",
+    price: "6 500 ₽",
+    category: "Гортензии",
+    image: "images/samarskaya-lidiya-big.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Пиксио",
+    price: "1 500 ₽",
+    category: "Гортензии",
+    image: "images/pixio.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Бонфаер",
+    price: "1 500 ₽",
+    category: "Гортензии",
+    image: "images/bonfire.jpg"
+},
+
+{
+    name: "Гортензия метельчатая Диамантино",
+    price: "1 800 ₽",
+    category: "Гортензии",
+    image: "images/diamantino.jpg"
+},
+
+/* КРУПНОЛИСТНЫЕ */
+
+{
+    name: "Гортензия крупнолистная Джип Блю",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/jip-blue.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Графиня Косель",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/grafinya-kosel.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Фаболо Блю",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/fabolo-blue.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Эрли Блю",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/early-blue.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Конфети Пантер",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/confetti-panther.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Стар Гезер",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/star-gazer.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Тугезер Блю",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/together-blue.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Ред Барон",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/red-baron.jpg"
+},
+
+{
+    name: "Гортензия крупнолистная Стелар Блю",
+    price: "2 000 ₽",
+    category: "Гортензии",
+    image: "images/stelar-blue.jpg"
+},
+
+/* ГОЛУБИКА */
+
+{
+    name: "Голубика сорт Либерти (с40л)",
+    price: "2 500 ₽",
+    category: "Голубика",
+    image: "images/liberty.jpg"
+},
+
+{
+    name: "Голубика сорт Река (с40)",
+    price: "2 500 ₽",
+    category: "Голубика",
+    image: "images/reka.jpg"
+},
+
+{
+    name: "Голубика сорт Дюк",
+    price: "2 500 ₽",
+    category: "Голубика",
+    image: "images/duke.jpg"
+},
+
+{
+    name: "Голубика сорт Дюк 40л",
+    price: "3 000 ₽",
+    category: "Голубика",
+    image: "images/duke40.jpg"
+},
+
+/* ПЛОДОВЫЕ */
+
+{
+    name: "Клубника сорт Тоскана",
+    price: "350 ₽",
+    category: "Плодовые",
+    image: "images/toskana.jpg"
+},
+
+{
+    name: "Яблоня Флорина",
+    price: "1 500 ₽",
+    category: "Плодовые",
+    image: "images/florina.jpg"
+},
+
+{
+    name: "Шелковица плакучая",
+    price: "4 500 ₽",
+    category: "Плодовые",
+    image: "images/shelkovica.jpg"
+},
+
+{
+    name: "Вишня Подбельская",
+    price: "600 ₽",
+    category: "Плодовые",
+    image: "images/podbelskaya.jpg"
+},
+
+{
+    name: "Вишня Изобильная",
+    price: "600 ₽",
+    category: "Плодовые",
+    image: "images/izobilnaya.jpg"
+},
+
+{
+    name: "Абрикос краснощекий",
+    price: "1 500 ₽",
+    category: "Плодовые",
+    image: "images/abrikos.jpg"
+},
+
+{
+    name: "Груша Конференция",
+    price: "1 500 ₽",
+    category: "Плодовые",
+    image: "images/conference.jpg"
+},
+
+{
+    name: "Груша Толгантская красавица",
+    price: "1 500 ₽",
+    category: "Плодовые",
+    image: "images/tolganskaya.jpg"
+},
+
+{
+    name: "Слива Анжелина",
+    price: "1 500 ₽",
+    category: "Плодовые",
+    image: "images/angelina.jpg"
+},
+
+{
+    name: "Слива Чернослив",
+    price: "1 500 ₽",
+    category: "Плодовые",
+    image: "images/chernosliv.jpg"
+},
+
+/* КУСТАРНИКИ */
+
+{
+    name: "Бирючина пестролистная",
+    price: "1 500 ₽",
+    category: "Кустарники",
+    image: "images/biryuchina.jpg"
+},
+
+{
+    name: "Сирень Аметист на штамбе",
+    price: "2 000 ₽",
+    category: "Кустарники",
+    image: "images/ametist.jpg"
+},
+
+{
+    name: "Сирень Шарль Жоли на штамбе",
+    price: "3 000 ₽",
+    category: "Кустарники",
+    image: "images/sharl-joli.jpg"
+},
+
+{
+    name: "Сирень Шарль Жоли",
+    price: "2 000 ₽",
+    category: "Кустарники",
+    image: "images/sharl-joli-2.jpg"
+},
+
+{
+    name: "Сирень Сенсация",
+    price: "700 ₽",
+    category: "Кустарники",
+    image: "images/sensaciya.jpg"
+},
+
+/* ХВОЙНЫЕ */
+
+{
+    name: "Туя Самарагд 1,2м",
+    price: "4 500 ₽",
+    category: "Хвойные",
+    image: "images/tuya-smaragd.jpg"
+},
+
+{
+    name: "Можжевельник Блю Эроу",
+    price: "1 300 ₽",
+    category: "Хвойные",
+    image: "images/blue-arrow.jpg"
+},
+
+{
+    name: "Можжевельник Лайм Глоу",
+    price: "1 500 ₽",
+    category: "Хвойные",
+    image: "images/lime-glow.jpg"
+},
+
+/* ДЕРЕВЬЯ */
+
+{
+    name: "Липа крупнолистная",
+    price: "5 500 ₽",
+    category: "Деревья",
+    image: "images/lipa.jpg"
+},
+
+{
+    name: "Дуб Красный",
+    price: "6 750 ₽",
+    category: "Деревья",
+    image: "images/dub.jpg"
+},
+
+{
+    name: "Ива пурпурная на штамбе",
+    price: "3 500 ₽",
+    category: "Деревья",
+    image: "images/iva.jpg"
+},
+
+{
+    name: "Боярышник Пауль Скарлет",
+    price: "3 500 ₽",
+    category: "Деревья",
+    image: "images/boyaryshnik.jpg"
+},
+
+/* МНОГОЛЕТКИ */
+
+{
+    name: "Гейхера Тирамису",
+    price: "500 ₽",
+    category: "Многолетки",
+    image: "images/geyhera.jpg"
+},
+
+{
+    name: "Георгины бордюрные",
+    price: "500 ₽",
+    category: "Многолетки",
+    image: "images/georginy.jpg"
+},
+
+{
+    name: "Гербера рассада",
+    price: "500 ₽",
+    category: "Многолетки",
+    image: "images/gerbera.jpg"
+},
+
+{
+    name: "Невянник",
+    price: "400 ₽",
+    category: "Многолетки",
+    image: "images/nevyannik.jpg"
+},
+
+{
+    name: "Хоста Подорожниковая С3",
+    price: "450 ₽",
+    category: "Многолетки",
+    image: "images/hosta.jpg"
+},
+
+/* ОДНОЛЕТКИ */
+
+{
+    name: "Колебрахоа рассада",
+    price: "320 ₽",
+    category: "Однолетки",
+    image: "images/kolebrahoa.jpg"
+},
+
+{
+    name: "Колеусы рассада",
+    price: "250 ₽",
+    category: "Однолетки",
+    image: "images/koleus.jpg"
+},
+
+{
+    name: "Цинерария рассада",
+    price: "40 ₽",
+    category: "Однолетки",
+    image: "images/cineraria.jpg"
+},
+
+{
+    name: "Петуния махровая рассада",
+    price: "650 ₽",
+    category: "Однолетки",
+    image: "images/petuniya.jpg"
+},
+
+{
+    name: "Фуксия",
+    price: "1 650 ₽",
+    category: "Однолетки",
+    image: "images/fuksiya.jpg"
+},
+
+{
+    name: "Гелиотроп рассада",
+    price: "400 ₽",
+    category: "Однолетки",
+    image: "images/geliotrop.jpg"
+},
+
+/* РОЗЫ */
+
+{
+    name: "Роза плетистая Лагуна",
+    price: "2 500 ₽",
+    category: "Розы",
+    image: "images/laguna.jpg"
+},
+
+{
+    name: "Роза Блю Маджента",
+    price: "2 500 ₽",
+    category: "Розы",
+    image: "images/blue-magenta.jpg"
+},
+
+{
+    name: "Роза Lichtkonigin Lucia",
+    price: "1 500 ₽",
+    category: "Розы",
+    image: "images/lichtkonigin.jpg"
+},
+
+{
+    name: "Лихткёниген Лючия",
+    price: "1 500 ₽",
+    category: "Розы",
+    image: "images/lucia.jpg"
+}
+
+];
